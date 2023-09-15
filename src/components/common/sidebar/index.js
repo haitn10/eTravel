08c9fd5827dcfe002e5713e6cb74dc506dafd20d
HaiTN <HaiTNSE150563@fpt.edu.vn>
2023-09-15 23:53:15 +0700

    [HaiTN] Fix sidebar for good UX

--- src/components/common/sidebar/index.js
+++ src/components/common/sidebar/index.js
@@ -1,90 +1,199 @@
-import { Box, IconButton, Typography, alpha, useTheme } from "@mui/material";
 import React, { useState } from "react";
-import { MenuOutline } from "@styled-icons/evaicons-outline";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Typography,
+  alpha,
+  useTheme,
+} from "@mui/material";
+import { Sidebar, Menu, SubMenu, MenuItem } from "react-pro-sidebar";
 
-import { Sidebar, Menu, SubMenu } from "react-pro-sidebar";
-import logo from "../../../assets/etravel-logo.png";
+import { MenuOutline } from "@styled-icons/evaicons-outline";
+import { ErrorOutline } from "@styled-icons/material";
+import { SignOut } from "@styled-icons/octicons";
+
 import Item from "./Item";
+import logo from "../../../assets/etravel-logo.png";
 import tabItems from "../../../constants/tabsItem";
+import { logOut } from "../../auth/action";
+import { useDispatch } from "react-redux";
 
 const SidebarApp = () => {
   const theme = useTheme();
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("Home");
   const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onLogout = () => {
+    dispatch(logOut());
+  };
+
   return (
-    <Sidebar
-      collapsed={isCollapsed}
-      backgroundColor={theme.palette.background.primary}
-    >
-      <Menu
-        iconShape="square"
-        menuItemStyles={{
-          root: {
-            color: theme.palette.text.third,
-          },
-
-          button: {
-            [`&:hover`]: {
-              backgroundColor: alpha(theme.palette.background.hovered, 0.05),
-            },
-          },
-        }}
+    <Box display="flex" flexDirection="column" justifyContent="space-between">
+      <Sidebar
+        collapsed={isCollapsed}
+        backgroundColor={theme.palette.background.primary}
       >
-        <Box
-          display="flex"
-          justifyContent={isCollapsed ? "center" : "flex-end"}
-          padding={1}
+        <Menu
+          iconShape="square"
+          menuItemStyles={{
+            root: {
+              color: theme.palette.text.third,
+            },
+            button: {
+              [`&:hover`]: {
+                backgroundColor: alpha(theme.palette.background.hovered, 0.025),
+              },
+            },
+          }}
         >
-          <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-            <MenuOutline width={24} />
-          </IconButton>
-        </Box>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent={isCollapsed ? "center" : "space-between"}
+            padding={1}
+          >
+            {!isCollapsed && (
+              <Box ml={2}>
+                <Typography
+                  color={theme.palette.text.third}
+                  fontWeight="medium"
+                >
+                  Tour Operator
+                </Typography>
+              </Box>
+            )}
 
-        {!isCollapsed && (
-          <Box mb="25px">
-            <Box display="flex" justifyContent="center" alignItems="center">
-              <img alt="Etravel Logo" width="150px" height="150px" src={logo} />
-            </Box>
+            <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+              <MenuOutline width={24} />
+            </IconButton>
           </Box>
-        )}
 
-        {tabItems.map((tab) => {
-          if (tab.options.length > 0) {
-            return (
-              <SubMenu
-                key={tab.id}
-                label={
-                  <Typography
-                    fontWeight={false ? "bold" : "medium"}
-                    color="inherit"
-                  >
-                    {tab.title}
-                  </Typography>
-                }
-                icon={tab.icon}
-              >
-                {tab.options.map((option) => (
-                  <Item
-                    key={option.id}
-                    showTitle={false}
-                    title={option.subTitle}
-                    icon={option.subIcon}
-                  />
-                ))}
-              </SubMenu>
-            );
-          } else {
-            return (
-              <Item
-                key={tab.id}
-                showTitle={isCollapsed}
-                title={tab.title}
-                icon={tab.icon}
-              />
-            );
-          }
-        })}
-      </Menu>
-    </Sidebar>
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="Etravel Logo"
+                  width="150px"
+                  height="150px"
+                  src={logo}
+                />
+              </Box>
+            </Box>
+          )}
+
+          {tabItems.map((tab) => {
+            if (tab.options.length > 0) {
+              return (
+                <SubMenu
+                  key={tab.id}
+                  label={
+                    <Typography
+                      fontWeight={false ? "bold" : "medium"}
+                      color="inherit"
+                    >
+                      {tab.title}
+                    </Typography>
+                  }
+                  icon={tab.icon}
+                >
+                  {tab.options.map((option, index) => (
+                    <Item
+                      key={index}
+                      title={option.subTitle}
+                      icon={option.subIcon}
+                      linkUrl={option.subUrl}
+                      subMenu={true}
+                      selected={selected === option.subTitle}
+                      setSelected={setSelected}
+                    />
+                  ))}
+                </SubMenu>
+              );
+            } else {
+              return (
+                <Item
+                  key={tab.id}
+                  title={tab.title}
+                  icon={tab.icon}
+                  linkUrl={tab.url}
+                  subMenu={false}
+                  selected={selected === tab.title}
+                  setSelected={setSelected}
+                />
+              );
+            }
+          })}
+        </Menu>
+      </Sidebar>
+      <Box marginBottom={3}>
+        <Sidebar
+          collapsed={isCollapsed}
+          backgroundColor={theme.palette.background.primary}
+        >
+          <Menu
+            iconShape="square"
+            menuItemStyles={{
+              root: {
+                color: theme.palette.text.third,
+              },
+              button: {
+                [`&:hover`]: {
+                  backgroundColor: alpha(
+                    theme.palette.background.hovered,
+                    0.025
+                  ),
+                },
+              },
+            }}
+          >
+            <MenuItem
+              icon={<ErrorOutline width={24} />}
+              onClick={() => setOpen(true)}
+            >
+              <Typography fontWeight="medium">Help & Information</Typography>
+            </MenuItem>
+            <MenuItem icon={<SignOut height={24} />} onClick={() => onLogout()}>
+              <Typography fontWeight="medium">Log out</Typography>
+            </MenuItem>
+          </Menu>
+        </Sidebar>
+      </Box>
+
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Help & Information"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            The system helps manage all application information. You can perform
+            basic operations to develop tours with the right voice for the
+            language.
+            <Typography fontWeight="medium">
+              <strong>Call us: </strong>
+              0971 520 977
+            </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="error" onClick={() => setOpen(false)} autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 };
 
